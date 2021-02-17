472293c3db43025fc921ba54570bf15b845391e2
FaziletKosure <faziletkosure1@gmail.com>
2021-02-18 00:07:54 +0100

    first content with view and text

--- App.js
+++ App.js
@@ -25,15 +25,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   carContainer:{
+    width:'100%',
+    height:'100%',
 
   },
   titles:{
-
+marginTop:'30%',
+width:'100%',
+alignItems:'center'
   },
   title:{
+    fontSize:40,
+    fontWeight:'500',
 
   },
   subtitle:{
+    fontSize:16,
+    color:'#5c5e62',
 
   },
 });

bf2f22b0ad36987c35fa7b52b2d21ee2a6aa26a5
FaziletKosure <faziletkosure1@gmail.com>
2021-02-18 14:03:44 +0100

    Created Custom Component

---
+++ components/CarItem/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, Text,ImageBackground} from 'react-native'
+import styles from "./styles"
+
+const CarItem=(props)=> {
+    return (
+        <View style={styles.carContainer}>
+        <ImageBackground  style={styles.image} source={require('../../assets/images/ModelX.jpeg')} /><View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,428</Text>
+        </View>
+        
+      </View>
+    )
+}
+
+export default CarItem
